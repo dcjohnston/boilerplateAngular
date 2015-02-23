367ebd20afb8378f4e6f25359b3730432a6f52ed
Derek Johnston <Derek@Dereks-MacBook-Pro.local>
2015-02-23 09:23:08 -0500

    set up main.js for AMD configuration

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+(function(){
+    require.config({
+        paths: {
+            'angular': '/../bower_components/angular/angular',
+            'angularAMD': '/../bower_components/angularAMD/angularAMD',
+            'ngload': '/../bower_components/angularAMD/ngload',
+            'ui-router': '/../bower_components/angular-ui-router/release/angular-ui-router',
+            'ngResource': '/../bower_components/angular-resource/angular-resource'
+        },
+        shim: {
+            'angularAMD': ['angular'],
+            'ngload': ['angularAMD'],
+            'ui-router': ['angular'],
+            'ngResource': ['angular']
+        },
+        deps: ['app']
+
+
+        
+    });
+})();
